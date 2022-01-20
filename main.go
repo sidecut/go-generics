bb7a623c36a5482a5a3071d785c096e36bdc2fd6
Jim Raden <radenj@ccf.org>
2022-01-20 16:30:22 -0500

    Add Number constraint (interface)

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 // SumInts adds together the values of m.
 func SumInts(m map[string]int64) int64 {
 	var s int64
@@ -20,9 +24,19 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
+// SumNumbers sums the values of map m. Its supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 // SumIntsOrFloats sums the values of map m. It supports both int64 and float64
 // as types for map values.
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
@@ -51,4 +65,7 @@ func main() {
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
 
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
